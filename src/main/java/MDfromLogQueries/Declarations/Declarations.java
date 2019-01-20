28457a47f8249111bed21bc447c9e7f8a44ef63f
pc <er_saidoune@esi.dz>
2019-01-20 11:32:21 +0100

    abstraction de la lecture et de l'ecriture dans un fichier dans la classe FileOperations

--- src/main/java/MDfromLogQueries/Declarations/Declarations.java
+++ src/main/java/MDfromLogQueries/Declarations/Declarations.java
@@ -6,8 +6,10 @@ public class Declarations {
  public  static String LogDirectory = "C:\\Users\\pc\\Desktop\\PFE\\DataLog\\dbp351logs\\";
  public  static String CleanedFile1 = "C:\\Users\\pc\\Desktop\\PFE\\Fichier_log_Nettoye_Complet.txt";
 
-    public static String directoryPath = "C:\\Users\\pc\\Desktop\\PFE\\DataLog\\OnlyLogs";
-    public static  String writingFilePath ="C:\\Users\\pc\\Desktop\\PFE\\Fichier_log_Nettoye_Complet_Parallel.txt";
+    //public static String directoryPath = "C:\\Users\\pc\\Desktop\\PFE\\DataLog\\OnlyLogs";
+    public static String directoryPath = "C:\\Users\\pc\\Desktop\\PFE\\DataLog\\test";
+    //public static  String writingFilePath ="C:\\Users\\pc\\Desktop\\PFE\\Fichier_log_Nettoye_Complet_Parallel.txt";
+    public static  String writingFilePath ="C:\\Users\\pc\\Desktop\\PFE\\Fichier_log_Nettoye_Complet_ParallelTest.txt";
 
     public static  String syntaxValidFile ="C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichier_Syntaxe_Valide.txt";
 
