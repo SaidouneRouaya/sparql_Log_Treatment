43d545b89cfb284902e8cb4d97c619402b44da3d
KamilaB <ek_boudoukha@esi.dz>
2019-01-30 20:00:32 +0100

    changements mineurs

--- src/main/java/MDfromLogQueries/Declarations/Declarations.java
+++ src/main/java/MDfromLogQueries/Declarations/Declarations.java
@@ -1,16 +1,16 @@
 package MDfromLogQueries.Declarations;
 
 public class Declarations {
-    public static String cleanedQueriesFile = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet_Parallel_test.csv";
-    //public static String cleanedQueriesFile = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichier_log_Nettoye.txt";
+    public static String cleanedQueriesFile = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet_Parallel_Scala.txt";
+    //public static String cleanedQueriesFile = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichier_log_Nettoye.txt";
  public  static String LogDirectory = "C:\\Users\\pc\\Desktop\\PFE\\DataLog\\dbp351logs\\";
- public  static String CleanedFile1 = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet.txt";
+ public  static String CleanedFile1 = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet.txt";
 
     public static String directoryPath = "C:\\Users\\KamilaB\\Desktop\\3CS\\Data log\\dbp351logs\\logtest";
-    public static  String writingFilePath ="C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet_Parallel_test.txt";
-    public static  String writingDedupFilePath = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichiers_Resultat\\Fichier_log_dedup_Nettoye.txt";
+    public static  String writingFilePath ="C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichiers_Resultat\\Fichier_log_Nettoye_Complet_Parallel_test.txt";
+    public static  String writingDedupFilePath = "C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichiers_Resultat\\Fichier_log_dedup_Nettoye_java.txt";
 
-    public static  String syntaxValidFile ="C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step 1\\Fichiers_Resultat\\Fichier_Syntaxe_Valide.txt";
+    public static  String syntaxValidFile ="C:\\Users\\KamilaB\\Desktop\\3CS\\Prototypage\\Step_1\\Fichiers_Resultat\\Fichier_Syntaxe_Valide.txt";
 
 
 }
