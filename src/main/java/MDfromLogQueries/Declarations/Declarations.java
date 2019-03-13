bc9c72a3428e09e4ceb6e9f647acff5812480ce9
SaidouneRouaya <er_saidoune@esi.dz>
2019-03-13 23:03:51 +0000

    voila

--- src/main/java/MDfromLogQueries/Declarations/Declarations.java
+++ src/main/java/MDfromLogQueries/Declarations/Declarations.java
@@ -1,37 +1,39 @@
 package MDfromLogQueries.Declarations;
 
 public class Declarations {
-    public static String cleanedQueriesFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_Scala.txt";
-    public static String cleanedQueriesFileCopie = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\FFichier_log_Nettoye_Complet_Parallel_Scala_Copie.txt";
+    public static String cleanedQueriesFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_Scala.txt";
+    public static String cleanedQueriesFileCopie = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\FFichier_log_Nettoye_Complet_Parallel_Scala_Copie.txt";
 
-    public static String LogDirectory = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Data log\\dbp351logs\\";
+    public static String LogDirectory = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Data log\\dbp351logs\\";
 
-    public static String CleanedFile1 = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet.txt";
+    public static String CleanedFile1 = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet.txt";
 
-    public static String directoryPath = "C:\\Users\\pc\\Desktop\\PFE\\Files\\DataLog\\test";
+    public static String directoryPath = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\DataLog\\test";
 
-    public static String directoryPathCopie = "C:\\Users\\pc\\Desktop\\PFE\\Files\\DataLog\\test - Copie";
-    public static String writingFilePath = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_test.txt";
-    public static String writingFilePathCopie = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_Java_Copie.txt";
+    public static String directoryPathCopie = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\DataLog\\test - Copie";
+    public static String writingFilePath = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_test.txt";
+    public static String writingFilePathCopie = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel_Java_Copie.txt";
 
-    public static String writingDedupFilePath = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_dedup_Nettoye.txt";
-    public static String modelsFilePath = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_models.rdf";
 
-    public static String syntaxValidFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_java.txt";
-    public static String syntaxValidFile2 = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_scala.txt";
-    public static String logFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log.txt";
-    public static String syntaxValidFileTest = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_Test.txt";
-    public static String constructQueriesFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Construct_scala.txt";
-    public static String statisticsFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Statistiques.txt";
+    public static String writingDedupFilePath = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log_Nettoye_Complet_Parallel.txt";
 
-    public static String syntaxNonValidFile = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Non_Valide_java.txt";
-    public static String syntaxNonValidFile2 = "C:\\Users\\pc\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Non_Valide_scala.txt";
+    public static String modelsFilePath = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_models.rdf";
 
-    public static String dbPediaOntologyPath = "C:\\Users\\pc\\Desktop\\PFE\\Files\\Ontologies\\onthology dbpedia\\dbpedia_2014.owl";
-    public static String defaultOntologiesDir = "C:\\Users\\pc\\Desktop\\PFE\\Files\\Ontologies\\ontologies_namespaces";
-    public static String tdbDirectory = "C:\\Users\\pc\\Desktop\\PFE\\Files\\tdbDirectory";
-    public static String originalTdbDirectory = "C:\\Users\\pc\\Desktop\\PFE\\Files\\originalTdbDirectory";
-    public static String propertiesOntology = "C:\\Users\\pc\\Desktop\\PFE\\Files\\Ontologies\\ontologies_namespaces\\addedOntology.ttl";
+    public static String syntaxValidFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_java.txt";
+    public static String syntaxValidFile2 = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_scala.txt";
+    public static String logFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_log.txt";
+    public static String syntaxValidFileTest = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Valide_Test.txt";
+    public static String constructQueriesFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Construct_scala.txt";
+    public static String statisticsFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Statistiques.txt";
+
+    public static String syntaxNonValidFile = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Non_Valide_java.txt";
+    public static String syntaxNonValidFile2 = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\ProgramOutput\\Fichier_Syntaxe_Non_Valide_scala.txt";
+
+    public static String dbPediaOntologyPath = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\Ontologies\\onthology dbpedia\\dbpedia_2014.owl";
+    public static String defaultOntologiesDir = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\Ontologies\\ontologies_namespaces";
+    public static String tdbDirectory = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\tdbDirectory";
+    public static String originalTdbDirectory = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\originalTdbDirectory";
+    public static String propertiesOntology = "C:\\Users\\roaya\\Desktop\\PFE\\Files\\Ontologies\\ontologies_namespaces\\addedOntology.ttl";
 
 
 }
